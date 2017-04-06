5ffac52795a4a4c30194846d24708b446031c5f2
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2017-04-06 18:58:33 +0200

    Easy add amount script add to mange.py

---
+++ cashier/management/commands/addTax.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+from cashier.models import Transaction, Room
+
+from datetime import date
+
+
+class Command(BaseCommand):
+
+    help = 'Adds the specified amount to all rooms'
+
+    def add_arguments(self, parser):
+        parser.add_argument('Amount', nargs='+', type=int)
+        parser.add_argument('Description', nargs='+')
+
+
+    def handle(self, *args, **options):
+        print(options)
+        rooms = Room.objects.all()
+        for room in rooms:
+            Appliedtax = Transaction(
+                                    date=str(date.today()),
+                                    amount=options['Amount'][0],
+                                    description=options['Description'][0],
+                                    room=room
+                        )
+            Appliedtax.save()
