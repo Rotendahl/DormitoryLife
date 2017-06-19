9cc87db0e7bb42a7eba18b3e64af59f434724f12
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2017-06-19 17:20:13 +0200

    Form works and creates dinnerclub but no transactions

--- cashier/views.py
+++ cashier/views.py
@@ -1,7 +1,7 @@
 """ Views for the cashier app """
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from cashier.models import Room
+from cashier.models import Room, Dinnerclub
 
 def all_rooms_overview(request):
     """ The view that shows the balance for all rooms """
@@ -28,4 +28,35 @@ def room_overview(request, room_nr):
 @login_required
 def add_dinner(request):
     """ Page to add dinnerclub """
-    return render(request, "cashier/AddDinner.html", {'data': {}})
+    if request.method == 'POST':
+        return handleDinnerClub(request)
+
+    rooms = Room.objects.all()
+    data = {'roomNrs': []}
+    for room in rooms:
+        data['roomNrs'].append(room.roomNr)
+    return render(request, "cashier/AddDinner.html", {'data': data})
+
+
+def handleDinnerClub(request):
+    """Takes a dinnerclub request and returns the probper template"""
+    fields = request.POST
+    if len(fields) != 6:
+        msg = {'status': "No participants added", 'error': True}
+        return render(request, "cashier/dinnerStatus.html", {'data' : msg})
+
+    for field in fields:
+        if fields[field] == '':
+            msg = {'status': "Field: " + field + " was empty", 'error': True}
+            return render(request, "cashier/dinnerStatus.html", {'data' : msg})
+
+    din_club = Dinnerclub(
+        date='-'.join(fields['Date'].split('-')[::-1]), # Fuck date formats
+        totalAmount=fields['Price'],
+        host=Room.objects.get(pk=fields['Host']),
+        menu=fields['Menu'],
+        )
+    print(din_club)
+    din_club.save()
+    msg = {'status': str(din_club), 'error': False}
+    return render(request, "cashier/dinnerStatus.html", {'data' : msg})
