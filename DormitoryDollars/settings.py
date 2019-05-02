ba51c04631a8114207007f971188944d6d9c1943
Benjamin <benjamin@rotendahl.dk>
2019-05-02 08:20:01 +0200

    Updated requirements and settings

--- DormitoryDollars/settings.py
+++ DormitoryDollars/settings.py
@@ -19,110 +19,103 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Sets production values
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 if SECRET_KEY is None:
     # Key used in development
     SECRET_KEY = "@232s&o6)$an1(m128jta(vhk#udv4ff-34lz=tdl58ahf(8zd"
 
-IS_PRODUCTION = bool(os.environ.get('DEBUG'))
+IS_PRODUCTION = bool(os.environ.get("DEBUG"))
 DEBUG = not IS_PRODUCTION
 
-ADMINS = [('Benjamin Rotendahl', 'Benjamin@Rotendahl.dk')]
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.rotendahl.dk'
+ADMINS = [("Benjamin Rotendahl", "Benjamin@Rotendahl.dk")]
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.rotendahl.dk"
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'dormitorydollars@rotendahl.dk'
-EMAIL_HOST_PASSWORD = os.environ.get('MAIL_KEY')
-SERVER_EMAIL = 'dormitorydollars@rotendahl.dk'
+EMAIL_HOST_USER = "dormitorydollars@rotendahl.dk"
+EMAIL_HOST_PASSWORD = os.environ.get("MAIL_KEY")
+SERVER_EMAIL = "dormitorydollars@rotendahl.dk"
 DEFAULT_FROM_EMAIL = SERVER_EMAIL
 
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
-    'bootstrap3',
-    'cashier',
-    'flat_responsive',
-    'django_extensions',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
+    "bootstrap3",
+    "cashier",
+    "flat_responsive",
+    "django_extensions",
 ]
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
 ]
 
-ROOT_URLCONF = 'DormitoryDollars.urls'
+ROOT_URLCONF = "DormitoryDollars.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
-            'debug': DEBUG,
+            "debug": DEBUG,
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'DormitoryDollars.wsgi.application'
+WSGI_APPLICATION = "DormitoryDollars.wsgi.application"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'da-dk'
-TIME_ZONE = 'Europe/Copenhagen'
+LANGUAGE_CODE = "da-dk"
+TIME_ZONE = "Europe/Copenhagen"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
 DB_FROM_ENV = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(DB_FROM_ENV)
+DATABASES["default"].update(DB_FROM_ENV)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
@@ -132,29 +125,30 @@ SECURE_BROWSER_XSS_FILTER = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
-X_FRAME_OPTIONS = 'DENY'
-ALLOWED_HOSTS = ['dormitorydollars.herokuapp.com', '1.0.0.0', 'www.oestervoldkollegiet.dk']
+X_FRAME_OPTIONS = "DENY"
+ALLOWED_HOSTS = [
+    "dormitorydollars.herokuapp.com",
+    "www.oestervoldkollegiet.dk",
+    "oestervoldkollegiet.dk",
+]
 
-if DEBUG: # pragma: no cover
+if DEBUG:  # pragma: no cover
     SECURE_CONTENT_TYPE_NOSNIFF = False
     SECURE_BROWSER_XSS_FILTER = False
     SESSION_COOKIE_SECURE = False
     CSRF_COOKIE_SECURE = False
     CSRF_COOKIE_HTTPONLY = False
-    X_FRAME_OPTIONS = 'ALLOW'
-    ALLOWED_HOSTS = ['*']
+    X_FRAME_OPTIONS = "ALLOW"
+    ALLOWED_HOSTS = ["*"]
     SECURE_HSTS_SECONDS = 300
     SECURE_SSL_REDIRECT = False
     SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
+STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, "static")]
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
