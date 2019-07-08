a80b4a1bb3504b1f90d598db3385adea56b8b8f1
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-07-08 12:07:54 +0200

    Added log out button

--- DormitoryDollars/settings.py
+++ DormitoryDollars/settings.py
@@ -149,6 +149,8 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 STATIC_URL = "/static/"
 
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, "static")]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
