6dd1d44a1d05146de619bd6c8c7865c2b0d2a000
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2019-09-19 17:50:07 +0200

    Also handles , as comma in soda price

--- cashier/views.py
+++ cashier/views.py
@@ -13,8 +13,8 @@ def add_drinks(request):
         for key in request.POST:
             if request.POST[key] != "":
                 data[key] = request.POST[key]
-        sodaPrice = float(data["price-soda"])
-        beerPrice = float(data["price-beer"])
+        sodaPrice = float(data["price-soda"].replace(',','.'))
+        beerPrice = float(data["price-beer"].replace(',','.')
         print(data)
         consumptions = [key for key in data.keys() if "consumed" in key]
 
